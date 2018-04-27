6f8a3df33654797b0fd3ba62c68fa8f28b4a4975
Leandro G. Ortega <leandro@g-ortega.com.ar>
2018-04-27 19:18:40 -0300

    hardcode de los limites del brazo

--- Tank/Arm.py
+++ Tank/Arm.py
@@ -1,14 +1,17 @@
 from Motor.Bone import Bone
 
+from time import time,sleep
+
+
 class Arm(object):
 	
 	def __init__(self, connectMotors = False):
 
 		self.skeleton = {
-			'FOOT'	: Bone('FOOT',	4, 0, 180, 90, 1050, 1950, False),
-			'Y1'	: Bone('Y1',	17, 0, 180, 90, 1050, 1950, False),
-			'Y2'	: Bone('Y2',	27, 0, 180, 90, 1050, 1950, False),
-			'Y3'	: Bone('Y3',	27, 0, 180, 90, 1050, 1950, False)
+			'FOOT'	: Bone('FOOT',	4, 0, 180, 60, 1050, 1950, False),
+			'Y1'	: Bone('Y1',	17, 40, 150, 150, 1050, 1950, False),
+			'Y2'	: Bone('Y2',	27, 0, 180, 0, 1050, 1950, False),
+			'Y3'	: Bone('Y3',	18, 0, 180, 90, 1050, 1950, False)
 		}
 		
 		self.powered = False
@@ -36,18 +39,18 @@ class Arm(object):
 			self.skeleton[bone].stop()
 
 	def setDirection(self, axis):
-		Y = axis.get('LEFT_Y')
-		X = axis.get('LEFT_X')
+		X = axis.get('RIGHT_X')
+		Y = axis.get('RIGHT_Y')
 
-		if (X > 0):
-			self.skeleton['FOOT'].incrementAngle()
-		elif (X < 0):
-			self.skeleton['FOOT'].decreaseAngle()
+		if (X < 0):
+			self.skeleton['FOOT'].incrementAngle(5)
+		elif (X > 0):
+			self.skeleton['FOOT'].decreaseAngle(5)
 
-		if (Y > 0):
-			self.skeleton['Y3'].incrementAngle()
-		elif (Y < 0):
-			self.skeleton['Y3'].decreaseAngle()
-
-arm = Arm(True)
+		if (Y < 0):
+			self.skeleton['Y1'].decreaseAngle(3)
+			self.skeleton['Y2'].incrementAngle(5)
+		elif (Y > 0):
+			self.skeleton['Y1'].incrementAngle(3)
+			self.skeleton['Y2'].decreaseAngle(5)
 
