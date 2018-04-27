e9512293588f920b420b89bd3f11bc92527a2f88
Leandro G. Ortega <leandro@g-ortega.com.ar>
2018-04-27 20:49:28 -0300

    Licencia GNU

--- Tank/Utils/functions.py
+++ Tank/Utils/functions.py
@@ -1,3 +1,19 @@
+'''
+This file is part of BalgassZord.
+
+    BalgassZord is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    BalgassZord is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with BalgassZord.  If not, see <http://www.gnu.org/licenses/>.
+'''
 
 ''' map(val, min, max, toMin, toMax) dado un valor lo escala segun min/max a toMin toMax '''
 def map(x, in_min, in_max, out_min, out_max):
